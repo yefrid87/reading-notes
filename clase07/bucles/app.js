7edd0eda843d056f2b5f174498448b2a7880a702
yefrid87 <titojeff87@gmail.com>
2024-10-11 12:52:06 -0500

    lab 7

--- clase07/bucles/app.js
+++ clase07/bucles/app.js
@@ -1,7 +1,16 @@
-while (true){
-    const pregunta =prompt("cuanto es la suma de horas trabajadas de 8+6")
-if (parseInt(pregunta)===14){
+
+   
+   
+    //pregunta al usuario
+    while (true){
+    const pregunta = prompt("cuanto es la suma de de 8+6")
+if (parseInt(pregunta) ===14){
     break;
 }
 }
-
+const cantidadDeGatos = prompt("cuantas veces quieres ver al gato");
+let contador = 0;
+while (contador <parseInt (cantidadDeGatos)){
+    document.write("<img src='./imagin/gatos.jpg' width='200'/>");
+    contador +=1;
+}
