09251f8d57c669d70765a101a1b2432496196412
yefrid87 <titojeff87@gmail.com>
2024-10-11 13:10:19 -0500

    final de lab 07

--- clase07/bucles/app.js
+++ clase07/bucles/app.js
@@ -12,5 +12,6 @@ const cantidadDeGatos = prompt("cuantas veces quieres ver al gato");
 let contador = 0;
 while (contador <parseInt (cantidadDeGatos)){
     document.write("<img src='./imagin/gatos.jpg' width='200'/>");
+    document.write("<h4>"+contador+>"</h4>");
     contador +=1;
 }
